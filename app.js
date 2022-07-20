827f6f9c223229f83fd86455c2a0b4571986e2ef
GabrielSup2 <gabriel_ppedro@hotmail.com>
2022-07-20 13:19:47 -0300

    Search for a task code

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const formAddTodo = document.querySelector(".form-add-todo");
 const UlsTodo = document.querySelector(".todos-container")
+const inputFindTodo = document.querySelector(".form-search")
 
 
 
@@ -9,7 +10,7 @@ formAddTodo.addEventListener("submit", event=>{
 
   if(inputValueWithNoBlanckSpace.length){
 
-const insertTaskOnToDoList =    UlsTodo.innerHTML +=  ` <li class="list-group-item d-flex justify-content-between align-items-center">
+const insertTaskOnToDoList  =    UlsTodo.innerHTML +=  ` <li class="list-group-item d-flex justify-content-between align-items-center">
 <span> ${inputValueWithNoBlanckSpace}</span>
 <i class="far fa-trash-alt delete"></i>
 </li>`
@@ -23,7 +24,9 @@ const insertTaskOnToDoList =    UlsTodo.innerHTML +=  ` <li class="list-group-it
 
 UlsTodo.addEventListener("click", event=>{
   itemClicked = event.target
+  
   const isAnIconTagName = itemClicked.tagName === "I" 
+
   if(isAnIconTagName){
     const parentI = itemClicked.parentNode
    parentI.remove()
@@ -32,4 +35,27 @@ UlsTodo.addEventListener("click", event=>{
 
 })
 
+inputFindTodo.addEventListener("input", (event)=>{
+
+inputValue = event.target.value.trim().toLowerCase()
+
+
+
+
+Array.from(UlsTodo.children).
+  filter(todo => !todo.textContent.normalize('NFD').replace(/[\u0300-\u036f]/g, "").toLowerCase().includes(inputValue.toLowerCase()) )
+  .forEach((todo)=>{
+    todo.classList.remove("d-flex")
+    todo.classList.add("hidden")
+  })
+ 
+  Array.from(UlsTodo.children).
+  filter(todo => todo.textContent.normalize('NFD').replace(/[\u0300-\u036f]/g, "").includes(inputValue.toLowerCase()) )
+  .forEach((todo)=>{
+    todo.classList.remove("hidden")
+    todo.classList.add("d-flex")
+  })
+  
+
+})
 
