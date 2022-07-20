c208c4648ceca74f1852edbb7d555c6579743cc0
GabrielSup2 <gabriel_ppedro@hotmail.com>
2022-07-20 12:25:28 -0300

    delete toDo Task code

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
 const formAddTodo = document.querySelector(".form-add-todo");
 const UlsTodo = document.querySelector(".todos-container")
 
+
+
 formAddTodo.addEventListener("submit", event=>{
   event.preventDefault();
-  const inputValue = event.target.add.value.trim()
+  const inputValueWithNoBlanckSpace = event.target.add.value.trim()
 
-  if(inputValue.length){
+  if(inputValueWithNoBlanckSpace.length){
 
-    UlsTodo.innerHTML +=  ` <li class="list-group-item d-flex justify-content-between align-items-center">
-    <span> ${inputValue}</span>
-    <i class="far fa-trash-alt delete"></i>
-    </li>`
+const insertTaskOnToDoList =    UlsTodo.innerHTML +=  ` <li class="list-group-item d-flex justify-content-between align-items-center">
+<span> ${inputValueWithNoBlanckSpace}</span>
+<i class="far fa-trash-alt delete"></i>
+</li>`
 
-    event.target.reset()
+    insertTaskOnToDoList
+     event.target.reset()
 
   }
 
-
-
 })
 
+UlsTodo.addEventListener("click", event=>{
+  itemClicked = event.target
+  const isAnIconTagName = itemClicked.tagName === "I" 
+  if(isAnIconTagName){
+    const parentI = itemClicked.parentNode
+   parentI.remove()
+   
+  }
 
+})
 
 
